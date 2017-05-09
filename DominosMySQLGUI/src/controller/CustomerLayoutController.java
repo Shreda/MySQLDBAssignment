da34acf19c8019bde51f99da2bb8ff6da638077a
Daniel <Daniel@DESKTOP-IU257DA.gateway>
2017-05-10 07:24:45 +1000

    Worked on CustomerDAO and the controller

--- DominosMySQLGUI/src/controller/CustomerLayoutController.java
+++ DominosMySQLGUI/src/controller/CustomerLayoutController.java
@@ -1,5 +1,45 @@
 package controller;
 
+import java.sql.SQLException;
+
+import javafx.collections.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import model.Customer;
+import model.CustomerDAO;
+
 public class CustomerLayoutController {
+	@FXML
+	private TableView<Customer> customerTable;
+	@FXML
+	private TableColumn<Customer, Integer> customerIDColumn;
+	@FXML
+	private TableColumn<Customer, String> customerEmailColumn;
+	@FXML
+	private TableColumn<Customer, String> customerPasswordColumn;
+	@FXML
+	private TableColumn<Customer, String> customerPhoneColumn;
+
+	@FXML
+	private void searchEmployees() throws SQLException {
+		ObservableList cList = CustomerDAO.searchCustomers();
+		populateTable(cList);
+	}
+
+	private void populateTable(ObservableList cList) {
+		customerTable.setItems(cList);
+	}
+
+	// This method is automatically called when the FXML file is loaded
+	// we set which field of the Customer objects the TableColumns should use as
+	// their data:
+	@FXML
+	private void initialize() throws SQLException {
+		customerIDColumn.setCellValueFactory(cellData -> cellData.getValue().customerIDProperty().asObject());
+		customerEmailColumn.setCellValueFactory(cellData -> cellData.getValue().customerEmailProperty());
+		customerPasswordColumn.setCellValueFactory(cellData -> cellData.getValue().customerPasswordProperty());
+		customerPhoneColumn.setCellValueFactory(cellData -> cellData.getValue().customerPhoneProperty());
+		searchEmployees();
+	}
 
 }
